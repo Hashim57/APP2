cc9eb16fa8590cfc665ef875b8e7cf173f701bea
Hashim57 <jordon57@icloud.com>
2020-09-23 15:15:26 +0100

    bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/bash
+
+function bootstrap(){
+    mkdir "$1"
+    cd "$1"
+    mkdir src tests
+    cd src
+    mkdir core
+    cd ..
+    python -m venv .venv
+    git init
+    touch .gitignore
+    echo "__pycache" >> ./.gitignore
+    echo ".venv" >> ./.gitignore
+    echo ".vscode" >> ./.gitignore
+    echo ".idea" >> ./.gitignore
+    echo "*.txt" >> ./.gitignore
+    echo "*csv" >> ./.gitignore
+    echo ".env" >> ./.gitignore
+    git add .
+    git commit -m "bootstrap"
+    cd "$1"
+    code .
+
+
+}
+
+# if [[ -d "$1"]]; then
+#     echo "Project Alreday Exists"
+# else
+bootstrap $1
+# fi 
+
+# exit 
